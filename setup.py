deca27e53b49e1f316606dea60b89cb2db454bc5
AVA00088 <ava00088@C7-FC-20.ava.local>
2018-05-16 15:13:03 -0400

    initial cookiecutter https://github.com/drivendata/cookiecutter-data-science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Exploring Text Preprocessing Techniques',
+    author='Kjell Wooding',
+    license='MIT',
+)
